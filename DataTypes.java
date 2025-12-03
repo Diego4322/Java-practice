19f3d9fbea645b44e8edba48e222fb19f4752744
Diego4322 <gldenpvp1@gmail.com>
2025-12-03 13:29:14 +0000

    Running INT

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+//Java is a STRONGLY TYPED language; you have to DEFINE/DECLARE a variable
+//you have to declare its data type
+
+class DataTypeExamples{
+    int myInt = 42;
+    double myDouble = 189.75; //twice as much as the integer
+    char myChar = 'Q';
+    boolean myBool = false;
+
+
+
+
+
+}
+public class DataTypes {
+    
+    public static void main(String[] args) { //tester class
+    DataTypeExamples dt = new DataTypeExamples();
+
+    System.out.println("myInt: " + dt.myInt);
+    }
+}
