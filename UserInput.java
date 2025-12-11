99f876bf352db47f2ae687fea6cc07d3112a7ead
Diego4322 <gldenpvp1@gmail.com>
2025-12-11 15:07:25 +0000

    User input

---
+++ UserInput.java
@@ -0,0 +1,29 @@
+//Scanner library
+import java.security.DigestOutputStream;
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter your name ");
+
+    String name = input.nextLine();
+    System.out.println(name);
+    
+    System.out.print("Enter your age ");
+    String age = input.nextLine();
+    System.out.println(age);
+
+    int ageConverted = Integer.parseInt(age);
+    System.out.println("next year " + name + " will be " + (ageConverted + 1)); //to make the ageConverted + 1 work you must put it into parenthesis
+
+    System.out.println("Whats your height in meters?");
+    double height = Double.parseDouble(input.nextLine());
+
+    System.out.println("You are " + height + " meters tall.");
+
+    }
+}
